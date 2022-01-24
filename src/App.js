e1cf3cd775f853a79fa559358f549d4a49202b09
LukacTomas <lukactv@gmail.com>
2022-01-24 06:00:28 +0100

    Class component changed to function compont

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import {
   Grid,
@@ -14,104 +14,76 @@ import {
 import PokemonRow from "./Components/PokemonRow";
 import PokemonInfo from "./Components/PokemonInfo";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filter: "",
-      pokemons: [],
-      selectedItem: null,
-    };
-  }
+function App() {
+  const [filter, setFilter] = useState("");
+  const [pokemons, setPokemons] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
 
-  setPokemons(pokemons) {
-    this.setState({
-      ...this.state,
-      pokemons,
-    });
-  }
-
-  setFilter(aString) {
-    this.setState({
-      ...this.state,
-      filter: aString,
-    });
-  }
-
-  setSelectedItem(selectedItem) {
-    this.setState({
-      ...this.state,
-      selectedItem,
-    });
-  }
-
-  componentDidMount() {
+  React.useEffect(() => {
     fetch("http://localhost:3000/starting-react/pokemon.json")
       .then((r) => r.json())
-      .then((pokemons) => this.setPokemons(pokemons));
-  }
+      .then((pokemons) => setPokemons(pokemons));
+  }, []);
 
-  render() {
-    return (
-      <Grid
-        container
-        justifyContent="center"
-        alignItems="center"
+  return (
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{
+        width: 800,
+        margin: "auto",
+      }}
+    >
+      <Typography color="primary" variant="h1" align="center">
+        Pokemon search
+      </Typography>
+      <TextField
+        label="Filter pokemons"
+        variant="filled"
+        value={filter}
         style={{
-          width: 800,
-          margin: "auto",
+          width: "100%",
         }}
-      >
-        <Typography color="primary" variant="h1" align="center">
-          Pokemon search
-        </Typography>
-        <TextField
-          label="Filter pokemons"
-          variant="filled"
-          value={this.state.filter}
-          style={{
-            width: "100%",
-          }}
-          onChange={({ target }) => this.setFilter(target.value.toLowerCase())}
-        />
-        <Grid container spacing={2}>
-          <Grid item sm={12} md={8}>
-            <Table width="100%">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Type</TableCell>
-                  <TableCell>Selection</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {this.state.pokemons
-                  .filter(({ name }) =>
-                    name.english.toLowerCase().includes(this.state.filter)
-                  )
-                  .slice(0, 20)
-                  .map((pokemon) => (
-                    <PokemonRow
-                      key={pokemon.id}
-                      pokemon={pokemon}
-                      onSelect={(pokemon) => this.setSelectedItem(pokemon)}
-                    />
-                  ))}
-              </TableBody>
-            </Table>
-          </Grid>
-          <Grid item sm={12} md={4}>
-            {this.state.selectedItem && (
-              <PokemonInfo
-                {...this.state.selectedItem}
-                onClose={() => this.setSelectedItem(null)}
-              />
-            )}
-          </Grid>
+        onChange={({ target }) => setFilter(target.value.toLowerCase())}
+      />
+      <Grid container spacing={2}>
+        <Grid item sm={12} md={8}>
+          <Table width="100%">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Type</TableCell>
+                <TableCell>Selection</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {pokemons
+                .filter(({ name }) =>
+                  name.english.toLowerCase().includes(filter)
+                )
+                .slice(0, 20)
+                .map((pokemon) => (
+                  <PokemonRow
+                    key={pokemon.id}
+                    pokemon={pokemon}
+                    onSelect={(pokemon) => setSelectedItem(pokemon)}
+                  />
+                ))}
+            </TableBody>
+          </Table>
+        </Grid>
+        <Grid item sm={12} md={4}>
+          {selectedItem && (
+            <PokemonInfo
+              {...selectedItem}
+              onClose={() => setSelectedItem(null)}
+            />
+          )}
         </Grid>
       </Grid>
-    );
-  }
+    </Grid>
+  );
 }
 
 export default App;
